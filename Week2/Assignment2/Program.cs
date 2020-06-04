2019a4b98edf40b61c113fb3e300f6ce2483ccdb
Oscar Rivera <66331387+Oscar-Rivera-KOL@users.noreply.github.com>
2020-06-04 21:36:02 +0200

    Folder Organized

---
+++ Week2/Assignment2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingAssignment2
+{
+    /// <summary>
+    /// Programming Assignment 2 solution
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Deals 2 cards to 3 players and displays cards for players
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            // print welcome message
+                Console.WriteLine("Dear User. Please be welome to our Game: Blackjack!!");
+
+            // create and shuffle a deck
+               
+
+            // deal 2 cards each to 3 players (deal properly, dealing
+            // the first card to each player before dealing the
+            // second card to each player)
+
+            // flip all the cards over
+
+            // print the cards for player 1
+
+            // print the cards for player 2
+
+            // print the cards for player 3
+
+            Console.WriteLine();
+        }
+    }
+}
